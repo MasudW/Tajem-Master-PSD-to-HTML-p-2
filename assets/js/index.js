400eb72a7836e637d970c3d77a09f072d0bbc931
Md Majidul Islam Masud <85607318+MasudW@users.noreply.github.com>
2022-06-27 12:49:33 +0600

    Project-2

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  /* ------------- navbar ------------- */
+  $(window).scroll(function () {
+    if (this.scrollY > 20)
+      $('.navbar').addClass('stciky');
+    else
+    $('.navbar').removeClass('stciky');
+  });
+  /* ------------- navbar toggler------------- */
+  $('.toogle-img').click(function () {
+    $(this).toggleClass('toggler');
+    $('.navbar').toggleClass('togle-bg');
+    $('.menu-area').toggleClass('menu-area-switch');
+  });
+  $('.nav-links').click(function () { 
+    $('.toogle-img').removeClass('toggler');
+    $('.menu-area').removeClass('menu-area-switch');
+    $('.navbar').removeClass('togle-bg');
+  });
+  /* --------------- js --------------- */
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      autoplay: {
+        delay: 5000,
+      },
+    });
+});
